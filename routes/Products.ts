697d500e124d29f3ab9153ae1ad71e3aed075a6b
Sammy <sammy.merazga@tempo-one.com>
2022-02-04 18:08:38 +0100

    pushEndSchool

--- routes/Products.ts
+++ routes/Products.ts
@@ -38,5 +38,78 @@ router.post(`/api/createProduct`, async (req: any, res: any) => {
   res.json(createProduct);
 });
 
+// CATEGORIES
+
+router.get("/api/homeProducts", async (req: any, res: any) => {
+  const homeProducts = await prisma.product.findMany({
+    select: {
+      id: true,
+      name: true,
+      price: true,
+      description: true,
+      description2: true,
+      image: true,
+      quantity: true,
+    },
+    where: {
+      category: "Maison"
+    }
+  });
+  res.status(200).send(homeProducts);
+});
+
+router.get("/api/productCategory", async (req: any, res: any) => {
+  const productCategory = await prisma.product.findMany({
+    select: {
+      id: true,
+      name: true,
+      price: true,
+      description: true,
+      description2: true,
+      image: true,
+      quantity: true,
+    },
+    where: {
+      category: "Produits"
+    }
+  });
+  res.status(200).send(productCategory);
+});
+
+router.get("/api/packs", async (req: any, res: any) => {
+  const packs = await prisma.product.findMany({
+    select: {
+      id: true,
+      name: true,
+      price: true,
+      description: true,
+      description2: true,
+      image: true,
+      quantity: true,
+    },
+    where: {
+      category: "Packs"
+    }
+  });
+  res.status(200).send(packs);
+});
+
+router.get("/api/others", async (req: any, res: any) => {
+  const others = await prisma.product.findMany({
+    select: {
+      id: true,
+      name: true,
+      price: true,
+      description: true,
+      description2: true,
+      image: true,
+      quantity: true,
+    },
+    where: {
+      category: "Autres"
+    }
+  });
+  res.status(200).send(others);
+});
 
 export default router;
